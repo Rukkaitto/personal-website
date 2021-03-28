114310efa7de9173d1bf86834fd94ba599306297
Lucas Goudin <goudinlucas@gmail.com>
2021-03-28 18:13:17 +0200

    Moved spinner to bottom right

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import './animations.css';
 function App() {
   return (
     <div>
-      <div className={"flex flex-row justify-center h-screen bg-black"}>
-          <div className={"flex flex-col justify-center w-36"}>
+      <div className={"flex flex-row justify-center h-screen bg-black md:justify-end md:items-end"}>
+          <div className={"flex flex-col justify-center w-36 md:mr-20 md:mb-8"}>
               <img className={"spinning"} src={"images/spinner/ren.png"} alt={"ren"}/>
               <img className={"spinning delay"} src={"images/spinner/takeyourtime.png"} alt={"take your time"}/>
           </div>
