f1f58fa6a0180f076e3d5edbabb3fc9fe26055f8
Lucas Goudin <goudinlucas@gmail.com>
2021-03-28 10:47:16 +0200

    Added loading spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import './animations.css';
 
 function App() {
   return (
     <div>
-      <div className="text-red-500 flex justify-center">
-        Hello world!
+      <div className={"flex flex-row justify-center h-screen bg-black"}>
+          <div className={"flex flex-col justify-center w-36"}>
+              <img className={"spinning"} src={"images/spinner/ren.png"} alt={"ren"}/>
+              <img className={"spinning delay"} src={"images/spinner/takeyourtime.png"} alt={"take your time"}/>
+          </div>
       </div>
     </div>
   );
